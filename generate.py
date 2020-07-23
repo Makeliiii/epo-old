99fecff9fb5f7f1360267950a41dbf685c6af5de
jpajukan <jpajukan@gmail.com>
2020-07-23 17:42:39 +0300

    Epo templating

--- generate.py
+++ generate.py
@@ -9,5 +9,9 @@ def main():
     index_template=templateEnv.get_template("index_template.html")
     index_template.stream().dump("index.html")
 
+    epo_template = templateEnv.get_template("epo_template.html")
+    epo_template.stream().dump("epo.html")
+
+
 if __name__ == '__main__':
     main()
