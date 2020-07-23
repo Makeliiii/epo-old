4ae5838977f7fefda7ca4920509be7896f5bc424
jpajukan <jpajukan@gmail.com>
2020-07-23 23:05:59 +0300

    Navbar active page fix

--- generate.py
+++ generate.py
@@ -7,13 +7,13 @@ def main():
     templateEnv = jinja2.Environment(loader=templateLoader)
     # Generate multiple pages
     index_template=templateEnv.get_template("index_template.html")
-    index_template.stream().dump("index.html")
+    index_template.stream(index_active=" active").dump("index.html")
 
     epo_template = templateEnv.get_template("epo_template.html")
-    epo_template.stream().dump("epo.html")
+    epo_template.stream(epo_active=" active").dump("epo.html")
 
     jaseneksi_template = templateEnv.get_template("jaseneksi_template.html")
-    jaseneksi_template.stream().dump("jaseneksi.html")
+    jaseneksi_template.stream(jaseneksi_active=" active").dump("jaseneksi.html")
 
 
 if __name__ == '__main__':
